38bccdd2cb92755bddf2e0c36f425f7031523068
Nihil Hesara <nihilhesa@gmail.com>
2024-08-23 12:05:02 +0530

    Generate password function

--- main.py
+++ main.py
@@ -4,33 +4,26 @@ import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-#Password Generator Project
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+    #Password Generator Project
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    num_letters = random.randint(8, 10)
+    num_symbols = random.randint(2, 4)
+    num_numbers = random.randint(2, 4)
 
-password_list = []
+    password_letters = [random.choice(letters) for _ in range(num_letters)]
+    password_symbol = [random.choice(symbols) for _ in range(num_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(num_numbers)]
 
-for char in range(nr_letters):
-  password_list.append(random.choice(letters))
+    password_list = password_letters + password_symbol + password_numbers
+    random.shuffle(password_list)
 
-for char in range(nr_symbols):
-  password_list += random.choice(symbols)
+    password = "".join(password_list)
 
-for char in range(nr_numbers):
-  password_list += random.choice(numbers)
-
-random.shuffle(password_list)
-
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+    print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
